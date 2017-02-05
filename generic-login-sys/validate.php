5499af1338e7006e1b0605c811aad106684b96f2
mbenba01 <m.benbaziz@hotmail.co.uk>
2017-02-05 20:28:40 +0000

    Made small change to the script

--- generic-login-sys/validate.php
+++ generic-login-sys/validate.php
@@ -1,23 +1,29 @@
 <?php
-
 	$myemail = "m.benbaziz@hotmail.co.uk";
 	$mypassword = "password";
-
-	if(isset($_POST['login'])) {
+	
+	if(isset($_POST['login'])) {	
 		
 		$email = $_POST['email'];
-		$password = $_POST['password'];
+		$password = $_POST['password'];	
 		
-		if($email == $myemail && $password == $mypassword) {
+		if($email == $myemail and $password == $mypassword) {
+				
 			if( isset($_POST['remember']) ) {
-				setcookie('email', $email, time()*60*60*24*7);
-				setcookie('password', $password, time()*60*60*24*7);
+				
+				setcookie('email', $email, time()+60*60*7);
+				setcookie('password', $password, time()+60*60*7);
+				
 			}
+			
 			session_start();
 			$_SESSION['email'] = $email;
 			header("location: welcome.php");
+			
 		} else {
+			
 			echo "Email or Password is Invalid. <br> click <a href=\"login.php\">here</a> to try again";
+			
 		}
 	} else {
 		header("location: login.php");
