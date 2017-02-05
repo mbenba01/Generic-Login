ad194aca1ce52eb5a9b6fffd5e9a7b5ce542d573
mbenba01 <m.benbaziz@hotmail.co.uk>
2017-02-05 19:43:18 +0000

    Small changes to validate.php

--- generic-login-sys/validate.php
+++ generic-login-sys/validate.php
@@ -10,8 +10,8 @@
 		
 		if($email == $myemail && $password == $mypassword) {
 			if( isset($_POST['remember']) ) {
-				setcookie('email', $email, time()*60*60*7);
-				setcookie('password', $password, time()*60*60*7);
+				setcookie('email', $email, time()*60*60*24*7);
+				setcookie('password', $password, time()*60*60*24*7);
 			}
 			session_start();
 			$_SESSION['email'] = $email;
