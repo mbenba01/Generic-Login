30e95f91839a68064439e3e05b1c09ccbdb000e8
mbenba01 <m.benbaziz@hotmail.co.uk>
2017-02-05 17:03:14 +0000

    set cookie to validate.php

--- generic-login-sys/validate.php
+++ generic-login-sys/validate.php
@@ -1,6 +1,19 @@
 <?php
+
+	$myemail = "m.benbaziz@hotmail.co.uk";
+	$mypassword = "password";
+
 	if(isset($_POST['login'])) {
 		
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		
+		if($email == $myemail && $password == $mypassword) {
+			$remember = $_POST['remember'];
+			setcookie('email', $email, time()*60*60*7);
+		} else {
+			echo "Email or Password is Invalid. <br> click <a href=\"login.php\">here</a> to try again";
+		}
 	} else {
 		header("location: login.php");
 	}
