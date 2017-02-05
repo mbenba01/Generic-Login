962c653b9d717eb78deb2e2c2cd2f5bf5303d6a4
mbenba01 <m.benbaziz@hotmail.co.uk>
2017-02-05 17:57:45 +0000

    Added a condition to set the cookie

--- generic-login-sys/validate.php
+++ generic-login-sys/validate.php
@@ -9,8 +9,10 @@
 		$password = $_POST['password'];
 		
 		if($email == $myemail && $password == $mypassword) {
-			$remember = $_POST['remember'];
-			setcookie('email', $email, time()*60*60*7);
+			if( isset($_POST['remember']) ):
+				setcookie('email', $email, time()*60*60*7);
+				header("location: welcome.php");
+			endif;
 		} else {
 			echo "Email or Password is Invalid. <br> click <a href=\"login.php\">here</a> to try again";
 		}
